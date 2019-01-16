0bcfceb10308ca898845aeeb6b69ef187a3b54b3
Kacper Mitkowski <kacper.mitkowski@gmail.com>
2019-01-16 11:22:56 +0100

    Adding added

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 from python import connection
 
 import uuid
 import csv
-from python import data_manager
+import shutil
+from python import data_manager, connection
 
 app = Flask(__name__)
 
@@ -20,6 +21,7 @@ def route_list():
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # KACPER'S FUNCTIONS
 
+# ADD QUESTION
 @app.route('/add-question', methods=['POST', 'GET'])
 def add_question():
 	
@@ -49,8 +51,51 @@ def add_question():
 
 	# ELSE (IF METHOD IS GET) -> SHOW THE FORM TO FILL
 	else:
-		return render_template('add_question.html') 
-							
+		return render_template('add_question.html',
+							   form_url = url_for('add_question')) 
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# EDIT QUESTION
+@app.route('/question/<question_id>/edit', methods=['POST','GET'])
+def edit_question(question_id):
+
+	if request.method == 'GET':
+		record = connection.get_data_by_id("sample_data/question.csv", question_id)
+		return render_template('add_question.html',
+								record = record,
+								form_url = url_for('edit_question', question_id = record['id']))
+	
+	else:
+		founded_row_as_dict = change_founded_row_by_id_into_dict(question_id)
+		headers_of_the_csv_table = ['id','submission_time','view_number','vote_number','title','message','image']
+		
+		with open('sample_data/question.csv', 'r') as csv_file, open('output_file.csv', 'w') as output:
+			reader = csv.DictReader(csv_file, fieldnames = headers_of_the_csv_table)
+			writer = csv.DictWriter(output, fieldnames = headers_of_the_csv_table)
+			for story in reader:
+				if story['id'] == question_id:
+					story = {k: founded_row_as_dict[k] for k in story}
+				writer.writerow(story)
+			shutil.move('output_file.csv', 'sample_data/question.csv')
+
+########################################################################################################################################
+# CHANGE THE ROW WITH GIVEN ID INTO DICTIONARY
+
+
+def change_founded_row_by_id_into_dict(id):
+
+    # VARIABLES FROM UPDATED FORM
+    question_title_from_form = request.form['question_title']
+    question_content_from_form = request.form['question_message']
+    question_url_img = request.form['url_address']
+
+    founded_row_transformed_into_dict = {'title': question_title_from_form, 
+                                         'message': question_content_from_form, 
+                                         'image': question_url_img}
+
+    return founded_row_transformed_into_dict
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 
