dc70c80c3216bd0fbbed76718b98007febfc5d15
Kacper Mitkowski <kacper.mitkowski@gmail.com>
2019-01-16 12:05:02 +0100

    Update done

--- server.py
+++ server.py
@@ -21,14 +21,17 @@ def route_list():
 
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
 def route_question_view(question_id):
-    single_question = connection.get_question_by_id(connection.QUESTION_FILE_PATH, question_id)
-    all_answers = connection.get_answers_by_id(connection.ANSWER_FILE_PATH, question_id)
+    single_question = connection.get_question_by_id(
+        connection.QUESTION_FILE_PATH, question_id)
+    all_answers = connection.get_answers_by_id(
+        connection.ANSWER_FILE_PATH, question_id)
     view_counter = int(single_question['view_number'])
     vote_counter = int(single_question['vote_number'])
     if request.method == 'GET':
         view_counter += 1
         single_question['view_number'] = str(view_counter)
-        connection.update_data_by_id(connection.QUESTION_FILE_PATH, connection.QUESTION_HEADER, question_id, single_question)
+        connection.update_data_by_id(
+            connection.QUESTION_FILE_PATH, connection.QUESTION_HEADER, question_id, single_question)
         return render_template('question_page.html',
                                single_question=single_question,
                                all_answers=all_answers)
@@ -47,104 +50,111 @@ def route_question_view(question_id):
 
 @app.route('/question/<question_id>/delete')
 def route_delete_question(question_id):
-    connection.delete_record_by_id(connection.QUESTION_FILE_PATH, connection.QUESTION_HEADER, question_id)
+    connection.delete_record_by_id(
+        connection.QUESTION_FILE_PATH, connection.QUESTION_HEADER, question_id)
     return redirect('/')
 
 
 @app.route('/answer/<answer_id>/delete')
 def route_delete_answer(answer_id):
-    connection.delete_record_by_id(connection.ANSWER_FILE_PATH, connection.ANSWER_HEADER, answer_id)
-    return redirect('/') #rozwiązać przekierowanie na stronę pytania
-	all_data = connection.get_all_data(connection.QUESTION_FILE_PATH)
-	headers = connection.QUESTION_HEADER
-	return render_template('questions_list.html',
-						   all_data = all_data,
-						   headers = headers)
+    connection.delete_record_by_id(
+        connection.ANSWER_FILE_PATH, connection.ANSWER_HEADER, answer_id)
+    return redirect('/')  # rozwiązać przekierowanie na stronę pytania
+    all_data = connection.get_all_data(connection.QUESTION_FILE_PATH)
+    headers = connection.QUESTION_HEADER
+    return render_template('questions_list.html',
+                           all_data=all_data,
+                           headers=headers)
 
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # KACPER'S FUNCTIONS
 
 # ADD QUESTION
 @app.route('/add-question', methods=['POST', 'GET'])
 def add_question():
-	
-	# IF METHOD IS POST (IF FORM IS SUBMITTED) -> THEN APPEND DATA IN CSV FILE
-	if request.method == 'POST':
 
-		question_title = request.form['question_title']
-		question_message = request.form['question_message']
-		question_url = request.form['url_address']
+        # IF METHOD IS POST (IF FORM IS SUBMITTED) -> THEN APPEND DATA IN CSV FILE
+    if request.method == 'POST':
 
-		question_actual_time = data_manager.generate_timestamp()
-		question_id = str(uuid.uuid1(clock_seq = question_actual_time))
+        question_title = request.form['question_title']
+        question_message = request.form['question_message']
+        question_url = request.form['url_address']
 
-		# SAVE TO CSV FILE 
-		# IT NEEDS TO BE CHANGED TO MAGDA'S FUNCTIONS (I DIDN'T KNOW HOW TO TRANSLATE CONTENT OF THE CSV FILE TO A DICT)	
-		with open('sample_data/question.csv', 'a', newline='') as csvfile:
-			headers_of_the_csv_table = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-			writer = csv.DictWriter(csvfile, fieldnames = headers_of_the_csv_table)
-			writer.writerow({'id': question_id,
-							'submission_time': question_actual_time, 
-							'view_number': 1, #IT NEEDS TO BE CHANGED
-							'vote_number': 2, #IT NEEDS TO BE CHANGED
-							'title': question_title, 
-							'message': question_message, 
-							'image': question_url})
-			return redirect('/question/' + question_id)
+        question_actual_time = data_manager.generate_timestamp()
+        question_id = str(uuid.uuid1(clock_seq=question_actual_time))
 
-	# ELSE (IF METHOD IS GET) -> SHOW THE FORM TO FILL
-	else:
-		return render_template('add_question.html',
-							   form_url = url_for('add_question')) 
+        # SAVE TO CSV FILE
+        # IT NEEDS TO BE CHANGED TO MAGDA'S FUNCTIONS (I DIDN'T KNOW HOW TO TRANSLATE CONTENT OF THE CSV FILE TO A DICT)
+        with open('sample_data/question.csv', 'a', newline='') as csvfile:
+            headers_of_the_csv_table = [
+                'id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+            writer = csv.DictWriter(
+                csvfile, fieldnames=headers_of_the_csv_table)
+            writer.writerow({'id': question_id,
+                             'submission_time': question_actual_time,
+                             'view_number': 1,  # IT NEEDS TO BE CHANGED
+                             'vote_number': 2,  # IT NEEDS TO BE CHANGED
+                             'title': question_title,
+                             'message': question_message,
+                             'image': question_url})
+            return redirect('/question/' + question_id)
+
+    # ELSE (IF METHOD IS GET) -> SHOW THE FORM TO FILL
+    else:
+        return render_template('add_question.html',
+                               form_url=url_for('add_question'))
 
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # EDIT QUESTION
-@app.route('/question/<question_id>/edit', methods=['POST','GET'])
+@app.route('/question/<question_id>/edit', methods=['POST', 'GET'])
 def edit_question(question_id):
+    record = connection.get_question_by_id("sample_data/question.csv", question_id)
+    if request.method == 'GET':
 
-	if request.method == 'GET':
-		record = connection.get_data_by_id("sample_data/question.csv", question_id)
-		return render_template('add_question.html',
-								record = record,
-								form_url = url_for('edit_question', question_id = record['id']))
-	
-	else:
-		founded_row_as_dict = change_founded_row_by_id_into_dict(question_id)
-		headers_of_the_csv_table = ['id','submission_time','view_number','vote_number','title','message','image']
-		
-		with open('sample_data/question.csv', 'r') as csv_file, open('output_file.csv', 'w') as output:
-			reader = csv.DictReader(csv_file, fieldnames = headers_of_the_csv_table)
-			writer = csv.DictWriter(output, fieldnames = headers_of_the_csv_table)
-			for story in reader:
-				if story['id'] == question_id:
-					story = {k: founded_row_as_dict[k] for k in story}
-				writer.writerow(story)
-			shutil.move('output_file.csv', 'sample_data/question.csv')
+        return render_template('add_question.html',
+                               record=record,
+                               form_url=url_for('edit_question', question_id=record['id']))
+
+    else:
+        id = record['id']
+        submission_time = record['submission_time']
+        view_number = record['view_number']
+        vote_number = record['vote_number']
+        title = request.form['question_title']
+        message = request.form['question_message']
+        image = request.form['url_address']
+
+        update_question = {'id' : id,
+                           'submission_time': submission_time,
+                           'view_number': view_number,
+                           'vote_number': vote_number,
+                           'title': title,
+                           'message': message,
+                           'image': image}
+
+        connection.update_data_by_id(connection.QUESTION_FILE_PATH, connection.QUESTION_HEADER, question_id, update_question)
+        return redirect('/')
 
 ########################################################################################################################################
 # CHANGE THE ROW WITH GIVEN ID INTO DICTIONARY
 
 
-def change_founded_row_by_id_into_dict(id):
+# def change_founded_row_by_id_into_dict(question_id):
 
-    # VARIABLES FROM UPDATED FORM
-    question_title_from_form = request.form['question_title']
-    question_content_from_form = request.form['question_message']
-    question_url_img = request.form['url_address']
+#     # VARIABLES FROM UPDATED FORM
+#     question_title_from_form = request.form['question_title']
+#     question_content_from_form = request.form['question_message']
+#     question_url_img = request.form['url_address']
 
-    founded_row_transformed_into_dict = {'title': question_title_from_form, 
-                                         'message': question_content_from_form, 
-                                         'image': question_url_img}
+#     founded_row_transformed_into_dict = {'id': question_id,
+#                                          'submission_time': 
+#                                          id,submission_time,view_number,vote_number,title,message,image}
 
-    return founded_row_transformed_into_dict
+#     return founded_row_transformed_into_dict
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-
-
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 if __name__ == "__main__":
